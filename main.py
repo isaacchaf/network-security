673952582232c7f66d7b60fe9b3351b234045f1c
isaacchaf <iechavez1998@gmail.com>
2024-12-11 21:17:10 -0600

    Mlflow evaluation

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
 from networksecurity.entity.config_entity import DataIngestionConfig, DataValidationConfig, DataTransformationConfig
 from networksecurity.entity.config_entity import TrainingPipelineConfig
+
+from networksecurity.components.model_trainer import ModelTrainer
+from networksecurity.entity.config_entity import ModelTrainerConfig
+
 import sys
 
 if __name__=='__main__':
@@ -28,6 +32,13 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_tranformation()
         print(data_transformation_artifact)
         logging.info("Data Transformation Competed")
+
+        logging.info("Model Trainig Started")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transfromation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+
+        logging.info("Mode Training Artifact Created")
         
     except Exception as e:
         raise NetworkSecurityException(e,sys)
